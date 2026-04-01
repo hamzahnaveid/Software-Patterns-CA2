b5538855db896d002982a9dd9c2e3731ea79de67
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2026-04-01 23:44:46 +0100

    Initial commit

---
+++ src/main/java/com/software_patterns/ca2/Ca2Application.java
@@ -0,0 +1,13 @@
+package com.software_patterns.ca2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ca2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ca2Application.class, args);
+	}
+
+}
