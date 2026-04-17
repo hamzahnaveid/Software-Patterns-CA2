eb808b3f07ce43834d051e2aae58b2ff3af08b64
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2026-04-18 00:06:02 +0100

    Delete product endpoint

--- src/main/java/com/software_patterns/ca2/controller/AdminProductController.java
+++ src/main/java/com/software_patterns/ca2/controller/AdminProductController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -82,5 +84,13 @@ public class AdminProductController {
 		products = sortService.sort(products, sortBy, type);
 		
 		return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@DeleteMapping("/delete-product/{productId}")
+	@ResponseBody
+	public ResponseEntity<Void> deleteProduct(@PathVariable int productId) {
+		productDao.deleteById(productId);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
